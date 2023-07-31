5178a83042e3c915723c9268de69a60c7869b2d3
Sifat-16 <sifatalamcep16@gmail.com>
2023-07-31 18:58:15 +0600

    Account module logic complete

--- lib/res/endpoints.dart
+++ lib/res/endpoints.dart
@@ -1,5 +1,12 @@
 class EndPoints{
 
-    final String all_posts = 'posts';
+    //final String base_url = 'http://127.0.0.1:8000/api/';
+    final String base_url = 'https://tetranos.com/api/';
+    final String login = 'user/login/';
+    final String signup = 'user/register/';
+    final String send_otp = 'otp/send-otp/';
+    final String verify_otp = 'otp/verify-otp/';
+    final String reset_password = 'user/reset-password/';
+
 
 }
